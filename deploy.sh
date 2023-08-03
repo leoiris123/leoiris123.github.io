6b300efdc75019f78643f764b2eab12dfd755345
lyrics <jijun.lei@mybogames.com>
2023-08-03 15:19:26 +0800

    deploy-1

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+
+set -e
+
+# 生成静态文件
+
+npm run build
+
+# 进入生成的文件夹
+
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'init: blog'
+
+git push -f <仓库的SSH地址> master:gh-pages
+
+cd -
